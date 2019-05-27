3bfd86be7f82067d7eed44fed8e8db3dfb3ee258
Jacob Ferlin <jacob.ferlin@jotex.se>
2019-05-27 16:02:57 +0200

    First try of rendering mdx query from mdx_tbl object.

--- R/render.R
+++ R/render.R
@@ -1,3 +1,42 @@
 render <- function(.data) {
-
+  render_cube(.data)
 }
+
+render_cube <- function(.data) {
+  paste0("[", find_cube(.data), "]")
+}
+
+#' Return Name of Cube
+find_cube <- function(.data) {
+  find_(.data, "cube")$info
+}
+
+#' Return First Operator with Name
+find_ <- function(.data, name) {
+
+  i      <- 1
+  i_op   <- NULL
+  i_name <- ""
+  while (i_name != name) {
+    i_op   <- op(.data, i)
+    i_name <- i_op$name
+    i <- i + 1
+    if (i > 20) break
+  }
+
+  i_op
+}
+
+#' Return Operator at Level
+op <- function(.data, level) {
+
+  op <- .data$ops
+  for (l in seq(1, level)) {
+    if (l == 1) next
+    op <- op$x
+  }
+
+  op
+}
+
+
