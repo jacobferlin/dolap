01f1c8afa65ad166bd7668e3b586b456a3344037
Jacob Ferlin <jacob.ferlin@jotex.se>
2019-06-03 15:34:48 +0200

    Print which filters are used. Print .data operators first, then query.

--- R/mdx_tbl.R
+++ R/mdx_tbl.R
@@ -16,17 +16,37 @@ collect.mdx_tbl <- function() {
 #' @importFrom pillar style_subtle
 print.mdx_tbl <- function(.data) {
 
-  tbl <- execute_query(.data)
-
   cat(style_subtle("# Cube:   "))
   cat(style_subtle(find_cube(.data)))
   cat(style_subtle("\n"))
 
-  cat(style_subtle("# Filter: "))
-  cat(style_subtle("ProductBusinessGroup == 'Jotex'"))
-  cat(style_subtle("\n"))
+  if (has_filters(.data)) {
+    filters_quos <- find_filters(.data)
+    filters_str  <- quos_expr_str(filters_quos)
 
+    cat(style_subtle("# Filter: "))
+
+    print_filter_seq <- seq(1, length(filters_str), 1)
+    for (print_filter_row in print_filter_seq) {
+      if (print_filter_row != 1) cat(style_subtle("#         "))
+      cat(style_subtle(filters_str[print_filter_row]))
+      cat(style_subtle("\n"))
+    }
+
+  }
+
+  cat(style_subtle("# Waiting for query..."))
+
+  tbl <- execute_query(.data)
+
+  cat(style_subtle("\r"))
   print(tbl)
+}
+
+quos_expr_str <- function(quos) {
+  lapply(quos, rlang::quo_name) %>%
+    unlist(use.names = FALSE) %>%
+    stringr::str_replace_all("\\\"", "'")
 }
 
 misc <- function() {
